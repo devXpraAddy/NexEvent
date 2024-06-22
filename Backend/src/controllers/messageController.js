53371dca95ed3b107e2ab4523c53529070a9cf13
Aditya Prajapati <princeak22486@gmail.com>
2024-06-22 13:04:19 +0530

    feat(backend): create message controller with basic CRUD operations

---
+++ Backend/src/controllers/messageController.js
@@ -0,0 +1,25 @@
+const Message = require("../models/messageSchema");
+
+const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const createMessage = async (req, res) => {
+  try {
+    const newMessage = new Message(req.body);
+    const savedMessage = await newMessage.save();
+    res.status(201).json(savedMessage);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getMessages,
+  createMessage,
+};
